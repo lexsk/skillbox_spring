d2644bafa0eb156b2a4cfcd6b2f314f392f2e090
skobeev.aleksey <skobeev.aleksey@yandex.ru>
2022-03-29 00:28:03 +0500

    Урок 3.3 Application Properties, профили и формат YAML

--- src/main/java/com/example/time_serv/TimeServApplication.java
+++ src/main/java/com/example/time_serv/TimeServApplication.java
@@ -1,11 +1,16 @@
 package com.example.time_serv;
 
+import com.example.time_serv.times.TimeProviderProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example"})
+@PropertySource(value = "classpath:custom.properties", encoding = "UTF-8")
+@EnableConfigurationProperties(TimeProviderProperties.class)
 public class TimeServApplication {
 
 	public static void main(String[] args) {
