16e75e33faaecd62f1c51bfa3e5c71b43401d71f
skobeev.aleksey <skobeev.aleksey@yandex.ru>
2022-03-28 23:31:41 +0500

    Урок 3.2 Spring Initializer

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO books(author, title, size)
-VALUES ('Dostoevsky', 'Idiot', 350);
-
-INSERT INTO users(username, password)
-VALUES ('root', '123');
-INSERT INTO users(username, password)
-VALUES ('user', '1');
-INSERT INTO users(username, password)
-VALUES ('1', '1');
