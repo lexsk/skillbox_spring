1ef1a22db426a0770fc3d975e629a1992bcba851
skobeev.aleksey <skobeev.aleksey@yandex.ru>
2022-03-29 22:22:34 +0500

    Урок 3.6 Подключение к базе данных были ошибки в исходном коде. В классе Book у полей price и priceOld стоял неверный тип Integer. Должен быть String. Также файлы создания БД изначально не корректные, необходимо их заменить отдельно приложенными.

--- src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -1,6 +1,9 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/bookshop")
 public class MainPageController {
 
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/main")
-    public String mainPage(){
+    public String mainPage(Model model){
+        model.addAttribute("bookData", bookService.getBooksData());
         return "index";
     }
 }
